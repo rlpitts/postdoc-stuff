7b2c1e11d282a2f434fc080f75260cbc656e274c
ColorOutOfSpace <20117570+rlpitts@users.noreply.github.com>
2023-10-06 10:20:14 +0200

    Update cyggauss4students.py docs
    Need to see if I can still access some of the files this function worked on.

--- cyggauss4students.py
+++ cyggauss4students.py
@@ -1,8 +1,17 @@
 '''
-
 Gaussian decomposition code from apphotcyg.py cleaned up for 1st year students.
 This one has an additional 7-component Gaussian function for N48.
 
+I was not familiar with most functools modules when I made this. I'm sure there's a
+better way to automate spawning of new Gaussian fits with partial() or decorators
+
+Requires a number of research-generated files to work: 
+- pilscygx-sma-ds9.reg (region file that describes circles around sources as seen by SMA) or
+	pilscygx-ellps-ds9.reg (describes ellipses around sources as seen by Herschel)
+- formerly proprietary science images of PILS-Cygnus objects with the SMA telescope, or
+	appropriately-sized cutouts of Herschel images
+This code has been tested both on SMA data and cutouts of Herschel data, with changes to the
+initial specs of the Gaussians for the latter.
 '''
 import numpy as np
 import glob#, math, sys
